92357142c59cf74cd6a18fab46cbe41422769db3
Czaroos <czaroosik@gmail.com>
2020-03-16 21:22:04 +0100

    state save with redux-persist test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store/index'
+import Store from './store/index'
 import App from './components/App';
 import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const { persistor, store } = Store();
 
 ReactDOM.render(
 <Provider store={store}>
+    <PersistGate loading={null}
+    persistor={persistor}>
     <App />
+    </PersistGate>
 </Provider>,
 document.getElementById("root")
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,18 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers'
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store = createStore(rootReducer);
 
-export default store;
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  let store = createStore(persistedReducer);
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
