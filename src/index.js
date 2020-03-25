99cc1bab70e09232bddebef86014b5492f987282
MichalzDev <zak.michal1997@gmail.com>
2020-03-25 22:52:52 +0100

    Listing tasks from in columns from db
    App version that tasks from MongoDB are displayed in proper columns also from DB. Merge from https://github.com/MichalzDev/Kanbak

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store/index'
-import App from './components/App';
 import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-<Provider store={store}>
-    <App />
-</Provider>,
-document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.unregister();
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import columnReducer from './columnReducer';
-
-export default combineReducers({
-    columns: columnReducer
-});
